e5e0c80b6e94907087893dc17e70df84b3e72cca
AndrusGerman <andrusdiazaleman@gmail.com>
2023-03-21 16:45:27 -0400

    Hello World

---
+++ 01-main.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  // this is a comment
+  /*
+  Other comment
+  */
+  print('Hello World');
+}
+
+
+
+// this function eq = dynamic main
+// main(List<String> args) {
+//   print('Hello World');
+// }
